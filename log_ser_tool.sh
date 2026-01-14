dc2f2d2ddfe386fb45ca0055453b76b8b6595de6
misiorrrroo <60035675+misiorrroo@users.noreply.github.com>
2026-01-14 10:38:01 +0100

    Added new file log_ser_tool.sh

---
+++ log_ser_tool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# log_ser_tool.sh
+# Created by: misiorrroo
+# Created on: 2026-01-14
+# Description: Add a brief description of the script's functionality here.
+
+# Main code starts below
+echo "Hello, world!"
+# End of script
